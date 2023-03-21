89fe9f44ece1f77363665ad297fd267717c0074d
TripleLin2020 <37936711+TripleLin2020@users.noreply.github.com>
2023-03-21 21:20:42 +0800

    Delete cmake-build-debug-wsl directory

--- cmake-build-debug-wsl/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lin/TinyJSON
-# Build directory: /home/lin/TinyJSON/cmake-build-debug-wsl
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("TinyJSON")
-subdirs("example")
--- cmake-build-debug-wsl/TinyJSON/CMakeFiles/TinyJSON.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "../lib/libTinyJSON.a"
-  "../lib/libTinyJSON.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/TinyJSON.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
